6f2b8de5e2aa4d139e1e30e9534a424d1d03d939
Anudev Gill <anudev.gill@mail.utoronto.ca>
2022-06-09 16:58:49 -0400

    Added necessary methods to NonAdminUser

--- phase0/LoginSystem/src/NonAdminUser.java
+++ phase0/LoginSystem/src/NonAdminUser.java
@@ -1,17 +1,82 @@
-public class NonAdminUser extends User implements Bannable{
-    @Override
-    public boolean checkUserBanStatus(User u) {
-        return false;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class NonAdminUser extends User implements Bannable {
+    private ArrayList<Calendar> loginTimes;
+    private boolean isBanned;
+    private String username;
+    private String password;
+
+    /**
+     * Creates an instance of a non-admin user.
+     * @param username this user's username.
+     * @param password this user's password.
+     */
+    public NonAdminUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+        isBanned = false;
+
+        loginTimes = new ArrayList<>();
+        loginTimes.add(Calendar.getInstance());
     }
 
-    @Override
-    public boolean temporarilyBanUser(User u) {
-        return false;
+    /**
+     * Logs this user in.
+     * @return true if the user has been successfully logged in.
+     */
+    public boolean logIn() {
+        loginTimes.add(Calendar.getInstance());
+        return true;
     }
 
-    @Override
-    public boolean unbanUser(User u) {
-        return false;
+    /**
+     * Return this user's login history.
+     * @return an arraylist of Calendar instances.
+     */
+    public ArrayList<Calendar> getLoginHistory() {
+        return loginTimes;
     }
 
+    /**
+     * Returns this user's username.
+     * @return username.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Returns this user's password.
+     * @return password.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Returns whether the user is currently banned.
+     * @return true if the user is banned and false if not.
+     */
+    public boolean checkUserBanStatus() {
+        return isBanned;
+    }
+
+    /**
+     * Bans user temporarily.
+     * @return true to signal the user has been banned.
+     */
+    public boolean temporarilyBanUser() {
+        isBanned = true;
+        return true;
+    }
+
+    /**
+     * Unbans user.
+     * @return true to signal the user has been unbanned.
+     */
+    public boolean unbanUser() {
+        isBanned = false;
+        return true;
+    }
 }
