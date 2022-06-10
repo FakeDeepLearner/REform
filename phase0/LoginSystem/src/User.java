37013ff00f5417a3936ca89cc9e86499ca0e53fe
rshetty22166@gmail.com <rshetty22166@gmail.com>
2022-06-09 20:55:56 -0400

    First commit for User Class. Intialized empty methods.

--- phase0/LoginSystem/src/User.java
+++ phase0/LoginSystem/src/User.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public abstract class User {
+    abstract boolean logIn();
+    abstract ArrayList<Calendar> getLoginHistory();
+
+    abstract String getUsername();
+
+    abstract String getPassword();
+
+    abstract boolean checkUserBanStatus();
+
+    abstract boolean temporarilyBanUser();
+
+    abstract boolean unbanUser();
+
 
 }
