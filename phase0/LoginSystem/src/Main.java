b8da5d6bd664e665c02dba8cf37d44589f265357
Andrew Chun <ay.chun@mail.utoronto.ca>
2022-06-12 19:26:21 -0400

    Format initialization of the program

--- phase0/LoginSystem/src/Main.java
+++ phase0/LoginSystem/src/Main.java
@@ -1,11 +1,35 @@
-import java.util.HashMap;
-import java.util.Scanner;
-
-public class Main extends HashMap<String ,String> {
-    public static void main(String[] args) throws InterruptedException {
+import java.util.Scanner;  //  import the Scanner class
+public class Main{
+    public static void main(String[] args) {
+        // Initiate the classes
         UserNameAndPasswordContainer<String, User> users = new UserNameAndPasswordContainer<String, User>();
+        // TODO: Load the database to <users>
         InterfaceManager manager = new InterfaceManager(users);
-        UserInterface userInterface = new UserInterface(manager);
+        UserInterface userInterface = new UserInterface();
+
+        InputHandler2 inputHandler = new InputHandler2();
+
         userInterface.printWelcomeMessage();
+
+        // Scanner has to be called in Main.java
+        Scanner sc = new Scanner(System.in);
+
+        boolean RUN = true;
+        while(RUN) {
+            String terminalInput = sc.nextLine();   // takes user input
+            inputHandler.takeInput(terminalInput);  // stores in input
+
+            if (inputHandler.isInt()){              // input is an integer
+                // TODO: <manager> preforms appropriate action based on the integer input
+            }
+            else{
+                // TODO: <manager> preforms appropriate action based on the input
+            }
+        }
+
+
+
+
+
     }
 }
