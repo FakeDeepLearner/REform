e30a8f7b76777cf3d688688d8e84cb92991f1d2e
Andrew Chun <ay.chun@mail.utoronto.ca>
2022-06-12 19:27:01 -0400

    Create InputHandler2.java

---
+++ phase0/LoginSystem/src/InputHandler2.java
@@ -0,0 +1,50 @@
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * This is a temporary class to test out alternative implementation of the InputHandler.
+ **/
+
+public class InputHandler2 {
+
+    private int intInput;
+    private String stringInput;
+    private boolean isInt;
+
+    public void takeInput(String input){
+
+        if (InputHandler2.isNumeric(input)){
+            this.intInput = Integer.parseInt(input);
+            this.isInt = true;
+        }
+        else{
+            this.stringInput = input;
+            this.isInt = false;
+        }
+    }
+
+    public static boolean isNumeric(String input) {
+        int n;
+
+        try {
+            n = Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+        }
+
+        return false;
+    }
+
+    public int getIntInput() {
+        return intInput;
+    }
+
+    public String getStringInput() {
+        return stringInput;
+    }
+
+    public boolean isInt() {
+        return isInt;
+    }
+
+}
