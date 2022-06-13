8374d26151cf5abe7ebb5a804f393d7ef743ac73
rshetty22166@gmail.com <rshetty22166@gmail.com>
2022-06-12 21:47:37 -0400

    Create User class commit

--- phase0/LoginSystem/src/CreateUser.java
+++ phase0/LoginSystem/src/CreateUser.java
@@ -1,7 +1,11 @@
 public class CreateUser {
-    private UserNameAndPasswordContainer<String, User> interface_users;
-
-    public CreateUser(UserNameAndPasswordContainer<String, User> interface_users) {
-        this.interface_users = interface_users;
+    public CreateUser(UserNameAndPasswordContainer<String, User> interface_users, String username, String password, Boolean isAdmin) {
+        User user;
+        if (isAdmin){
+            user = new AdminUser(username, password);
+        }else{
+            user = new NonAdminUser(username, password);
+        }
+        interface_users.put(username,user);
     }
 }
