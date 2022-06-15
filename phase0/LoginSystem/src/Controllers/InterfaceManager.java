011a7b21b8bb8bb587220a867ad392f0b807c32d
Felix Zhang <felixfzhang@hotmail.com>
2022-06-15 17:50:45 -0400

    Added functionality for users that are logged in

--- phase0/LoginSystem/src/Controllers/InterfaceManager.java
+++ phase0/LoginSystem/src/Controllers/InterfaceManager.java
@@ -4,7 +4,9 @@ import Entities.User;
 import Entities.UserNameAndPasswordContainer;
 import useCases.AuthenticateUser;
 import useCases.CreateUser;
+import useCases.UpdateUserHistory;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -15,6 +17,7 @@ public class InterfaceManager {
     private UserInterface ui;
     private AuthenticateUser auth;
     private CreateUser createUser;
+    private UpdateUserHistory history;
 
     public InterfaceManager(UserNameAndPasswordContainer<String, User> interfaceUsers) {
         this.interfaceUsers = interfaceUsers;
@@ -23,7 +26,9 @@ public class InterfaceManager {
         ui = new UserInterface(new InputHandler3());
         auth = new AuthenticateUser(interfaceUsers);
         createUser = new CreateUser(interfaceUsers);
+        history = new UpdateUserHistory(interfaceUsers);
     }
+
     private ArrayList<String> getUsernameAndPassword() {
         ArrayList<String> username_password = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
@@ -37,7 +42,13 @@ public class InterfaceManager {
         return username_password;
     }
 
-    public void menuSelector() {
+    public User menuSelector() {
+        try {
+            ui.printWelcomeMessage();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+
         ArrayList<Integer> allowedInputs = new ArrayList<Integer>();
         Collections.addAll(allowedInputs, 1, 2);
 
@@ -52,7 +63,13 @@ public class InterfaceManager {
 
             ui.printSignUpSuccess();
 
-            NonAdminScreen(interfaceUsers.get(username_password.get(0)));
+            auth.loginUser(username_password.get(0), username_password.get(1));
+            User u = interfaceUsers.get(username_password.get(0));
+            history.writeUserHistory(u, false);
+
+            return u;
+
+//            NonAdminScreen(interfaceUsers.get(username_password.get(0)));
         } else if (menuSelect == 2) {
             // User selected to create a new user
             ArrayList<String> username_password = getUsernameAndPassword();
@@ -61,15 +78,15 @@ public class InterfaceManager {
                 ui.printLoginSuccess();
 
                 User u = interfaceUsers.get(username_password.get(0));
-                if (!u.getAdmin()) {
-                    NonAdminScreen(u);
-                } else {
-                    AdminScreen(u);
-                }
+                history.writeUserHistory(u, false);
+
+                return u;
             } else {
                 ui.printLoginFail();
             }
         }
+
+        return null;
     }
 
     public void NonAdminScreen(User u) {
@@ -78,6 +95,22 @@ public class InterfaceManager {
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
         }
+
+        ArrayList<Integer> allowedInputs = new ArrayList<Integer>();
+        Collections.addAll(allowedInputs, 1, 2);
+
+        int select = inputHandler.intInput(allowedInputs);
+        switch (select) {
+            case 1:
+                // View login history
+                ArrayList<String> userHistory = u.getLoginHistory();
+                System.out.println(userHistory);
+                break;
+            case 2:
+                // Logout user
+                auth.logoutUser(u);
+                break;
+        }
     }
 
     public void AdminScreen(User u) {
@@ -85,6 +118,26 @@ public class InterfaceManager {
             ui.printAdminLoginMenu();
         } catch (InterruptedException e) {
             System.out.println("Interrupted");
+        }
+
+        ArrayList<Integer> allowedInputs = new ArrayList<Integer>();
+        Collections.addAll(allowedInputs, 1, 2, 3);
+
+        int select = inputHandler.intInput(allowedInputs);
+        switch (select) {
+            case 1:
+                // View login history
+                ArrayList<String> userHistory = u.getLoginHistory();
+                System.out.println(userHistory);
+                break;
+            case 2:
+                // Create admin user
+                ArrayList<String> username_password = getUsernameAndPassword();
+                createUser.createAdminUser(username_password.get(0), username_password.get(1));
+            case 3:
+                // Logout user
+                auth.logoutUser(u);
+                break;
         }
     }
 
