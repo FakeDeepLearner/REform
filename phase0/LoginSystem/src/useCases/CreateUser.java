f17dfa99e63937a23e47ae97ff651406dbd6b0d7
rshetty22166@gmail.com <rshetty22166@gmail.com>
2022-06-16 22:25:25 -0400

    Create User class exception fixed-commit

--- phase0/LoginSystem/src/useCases/CreateUser.java
+++ phase0/LoginSystem/src/useCases/CreateUser.java
@@ -4,6 +4,8 @@ import Entities.AdminUser;
 import Entities.NonAdminUser;
 import Entities.User;
 import Entities.UserNameAndPasswordContainer;
+import Exceptions.UserNotFoundException;
+import Exceptions.UsernameAlreadyExistsException;
 
 public class CreateUser {
     private UserNameAndPasswordContainer<String, User> interface_users;
@@ -13,12 +15,29 @@ public class CreateUser {
     }
 
     public void createNonAdminUser(String username, String password) {
-        User user = new NonAdminUser(username, password);
-        interface_users.put(username, user);
+        if(checkUsernameExists(username)){
+            User user = new NonAdminUser(username, password);
+            interface_users.put(username, user);
+        }else{
+            throw new UsernameAlreadyExistsException();
+        }
     }
 
     public void createAdminUser(String username, String password) {
-        User user = new AdminUser(username, password);
-        interface_users.put(username, user);
+        if(checkUsernameExists(username)){
+            User user = new AdminUser(username, password);
+            interface_users.put(username, user);
+        }else{
+            throw new UsernameAlreadyExistsException();
+        }
+    }
+
+    public boolean checkUsernameExists(String username) {
+        try {
+            User u = interface_users.get(username);
+            return true;
+        } catch (UserNotFoundException e) {
+            return false;
+        }
     }
 }
