89b5dd8f39b3c74005adeacc96f9aa6dd616ca9e
Anudev Gill <anudev.gill@mail.utoronto.ca>
2022-06-19 19:52:21 -0400

    Deleted unused exceptions

--- phase0/src/Exceptions/ReturnToMainMenuException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class ReturnToMainMenuException extends Exception{
-    public ReturnToMainMenuException(){
-        super("Returning to main menu, please wait.");
-    }
-}
