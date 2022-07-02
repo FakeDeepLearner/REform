3e8df727b272781afc94ce0380fe1fd8e4f09c18
Anudev Gill <anudev.gill@mail.utoronto.ca>
2022-07-01 20:37:27 -0400

    Created use case class CreateListing that has method to add a listing to csv file

---
+++ phase1/src/useCases/CreateListing.java
@@ -0,0 +1,39 @@
+package useCases;
+
+import entities.Listing;
+import java.io.*;
+import java.math.BigDecimal;
+
+public class CreateListing {
+    public void createListing(int id, int civicAddress, String streetName, String city, String type, int bedrooms,
+                              int bathrooms, BigDecimal price) {
+        Listing listing = new Listing(id, civicAddress, streetName, city, type, bedrooms, bathrooms, price);
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/Listings.csv", false));
+
+            bw.append(String.valueOf(id));
+            bw.append(",");
+            bw.append(String.valueOf(civicAddress));
+            bw.append(",");
+            bw.append(streetName);
+            bw.append(",");
+            bw.append(city);
+            bw.append(",");
+            bw.append(type);
+            bw.append(",");
+            bw.append(String.valueOf(bedrooms));
+            bw.append(",");
+            bw.append(String.valueOf(civicAddress));
+            bw.append(",");
+            bw.append(String.valueOf(price));
+            bw.append(",");
+            bw.append("\n");
+
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
