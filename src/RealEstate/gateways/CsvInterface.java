361553e2bdff6318a84ca00bde7ce0533b23ea4b
Anudev Gill <anudev.gill@mail.utoronto.ca>
2022-07-10 19:33:18 -0400

    Created CsvInterface

---
+++ src/RealEstate/gateways/CsvInterface.java
@@ -0,0 +1,14 @@
+package RealEstate.gateways;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class CsvInterface {
+    public void read(BufferedReader br) {
+
+    }
+
+    public void write(BufferedWriter bw) {
+
+    }
+}
