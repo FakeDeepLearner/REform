a494eabbda162e60129e6f2b7327317912482b27
Andrew Chun <ay.chun@mail.utoronto.ca>
2022-07-10 22:32:12 -0400

    update GenerateUniqueIDTest

--- test/GenerateUniqueIDTest.java
+++ test/GenerateUniqueIDTest.java
@@ -1,6 +1,32 @@
+import RealEstate.entities.ListingContainer;
+import RealEstate.useCases.GenerateUniqueID;
 import org.junit.*;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
 public class GenerateUniqueIDTest {
+
+    @Test
+    public void testGenerateUniqueID(){
+        ListingContainer l = new ListingContainer();
+        l.put(2, "key");
+        GenerateUniqueID a = new GenerateUniqueID(l);
+
+        a.setMinID(1);
+        a.setMaxID(4);
+
+        ArrayList<Integer> allowedInts = new ArrayList<>();
+        allowedInts.add(1);
+        allowedInts.add(3);
+        allowedInts.add(4);
+        int id = a.getUniqueID();
+
+        System.out.println(id);
+
+        assertTrue(allowedInts.contains(id));
+
+    }
 }
