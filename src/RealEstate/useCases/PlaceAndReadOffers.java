d0eb40b2e7d04cd6039591541b23f2ce19d066dd
rjak03 <ranajk03@gmail.com>
2022-07-10 20:36:02 -0400

    Changed implementation of sending messages and reading messages from file to include containers and added a csv file for the messages to get stored in.

--- src/RealEstate/useCases/PlaceAndReadOffers.java
+++ src/RealEstate/useCases/PlaceAndReadOffers.java
@@ -1,6 +1,15 @@
 package RealEstate.useCases;
 
+import LoginSystem.entities.Bannable;
+import LoginSystem.entities.NonAdminUser;
+import LoginSystem.entities.User;
+import LoginSystem.entities.UserContainer;
+import RealEstate.entities.Listing;
+import RealEstate.entities.Message;
+import RealEstate.entities.MessageContainer;
+
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,64 +17,74 @@ import java.util.Scanner;
 
 public class PlaceAndReadOffers {
 
-    /**
-     * Writes a new message to csv file.
-     *
-     * @param sendingUsername the sending user's username.
-     * @param receivingUsername the receiving user's username.
-     */
-    public void updateMessageHistory(String sendingUsername, String receivingUsername, String message) {
+    public final MessageContainer<Integer, Message> messageContainer;
+    public final UserContainer<String, User> userContainer;
+    public final DatabaseFilePath file;
 
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("src/UserMessages.csv", true));
-            Scanner sc = new Scanner("src/UserMessages.csv");
 
-            while (sc.hasNext()) {
-                sc.next();
-            }
+    public PlaceAndReadOffers(MessageContainer<Integer, Message> messageContainer,
+                              UserContainer<String, User> userContainer) {
+        this.messageContainer = messageContainer;
+        this.userContainer = userContainer;
+        file = new DatabaseFilePath("UserMessages.csv");
+    }
 
-            bw.append(sendingUsername);
-            bw.append(",");
-            bw.append(receivingUsername);
-            bw.append(",");
-            bw.append(message);
-            bw.append("\n");
+    public void sendMessage(String senderUsername, String recipientUsername, String content) {
 
-            bw.flush();
-            bw.close();
+        User sender = userContainer.get(senderUsername);
+        User recipient = userContainer.get(recipientUsername);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (sender instanceof Bannable && recipient instanceof Bannable) {
+            Message message = new Message(sender, recipient, 0, content); //change id to call id generating use case
+
+            messageContainer.put(0, message); //change id to call id generating use case
+
+            ((NonAdminUser)sender).sendMessage((NonAdminUser)recipient, message);
         }
     }
 
-    /**
-     * Reads the user's message history from the csv file.
-     *
-     * @param username this user's username.
-     * @return an arraylist of the received messages that include sender's username in the Strings.
-     */
-    public ArrayList<String> readUserMessageHistory(String username) {
-        ArrayList<String> userMessageHistory = new ArrayList<>();
+
+    public void readMessageHistory() {
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader("src/UserMessages.csv"));
+            BufferedReader br = new BufferedReader(new FileReader(file.getFilePath()));
             String line;
 
             while ((line = br.readLine()) != null) {
-                ArrayList<String> contents = new ArrayList<>(Arrays.asList(line.split(",")));
+                String[] contents = line.split(",");
+                User sender = userContainer.get(contents[0]);
+                User recipient = userContainer.get(contents[1]);
 
-                if (username.equals(contents.get(1))) {
-                    String message = String.join(",", contents.subList(2, contents.size()));
-                    userMessageHistory.add(contents.get(0) + ": " + message);
-                }
+                Message m = new Message(sender,recipient,Integer.parseInt(contents[2]),contents[3]);
+                messageContainer.put(Integer.parseInt(contents[2]),m);
             }
 
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        return userMessageHistory;
+    public void writeMessages(){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file.getFilePath(), true));
+
+            for(Message m : messageContainer.values()){
+
+                bw.append(m.getSender().getUsername());
+                bw.append(",");
+                bw.append(m.getRecipient().getUsername());
+                bw.append(",");
+                bw.append(String.valueOf(m.getMessageID()));
+                bw.append(",");
+                bw.append(m.getContents());
+                bw.append("\n");
+            }
+            bw.flush();
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
