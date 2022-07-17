b81e095e128c3c81101229b99a619956804d56ee
Felix Zhang <felixfzhang@hotmail.com>
2022-07-17 16:38:12 -0400

    Add Javadocs

--- src/LoginSystem/entities/AdminUser.java
+++ src/LoginSystem/entities/AdminUser.java
@@ -5,11 +5,24 @@ import RealEstate.entities.UserType;
 import java.util.ArrayList;
 
 public class AdminUser extends User {
+    /**
+     * Creates an instance of an AdminUser
+     *
+     * @param username of the User
+     * @param password of the User
+     */
     public AdminUser(String username, String password) {
         super(UserType.ADMIN, username, password);
         this.isAdmin = true;
     }
 
+    /**
+     * Creates an instance of an AdminUser
+     *
+     * @param username of the User
+     * @param password of the User
+     * @param loginHistory of the User
+     */
     public AdminUser(String username, String password, ArrayList<String> loginHistory) {
         super(UserType.ADMIN, username, password, loginHistory);
         this.isAdmin = true;
