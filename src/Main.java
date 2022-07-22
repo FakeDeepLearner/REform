f2e8d556662015b64abd3adbb73db01a8515dbbf
Felix Zhang <felixfzhang@hotmail.com>
2022-07-21 22:25:24 -0400

    Load admin users from CSV

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import LoginSystem.controllers.InputHandler;
 import LoginSystem.controllers.LoggedOutManager;
 import LoginSystem.controllers.UserInterface;
 import LoginSystem.controllers.UserManager;
+import LoginSystem.entities.AdminUser;
 import LoginSystem.entities.User;
 import LoginSystem.entities.UserContainer;
 import LoginSystem.exceptions.ExitProgramException;
@@ -19,6 +20,7 @@ import RealEstate.gateways.*;
 import RealEstate.useCases.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -29,6 +31,7 @@ public class Main {
         UserFactory userFactory = new UserFactory(users);
         AuthenticateUser auth = new AuthenticateUser(users);
         UpdateUserHistory history = new UpdateUserHistory(users);
+        UsernamePasswordFileEditor usernamePassword = new UsernamePasswordFileEditor(auth, users);
         RestrictUser restrict = new RestrictUser(users);
         UsernamePasswordFileEditor file = new UsernamePasswordFileEditor(auth, users);
         SendMessages sendMessages = new SendMessages(messages, users);
@@ -53,6 +56,14 @@ public class Main {
 
         buyersCSVController.read();
         sellersCSVController.read();
+
+        ArrayList<ArrayList<String>> usersPassword = usernamePassword.getUsersFromCSV();
+        for (ArrayList<String> userPassword : usersPassword) {
+            if (userPassword.get(2).equals("true")) {
+                userFactory.createUser("ADMIN", userPassword.get(0), userPassword.get(1));
+            }
+        }
+
         listingsCSVController.read();
         userMessagesCSVController.read();
         usersAndFavoritesCSVController.read();
