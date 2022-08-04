19ed2450d5edf48f8a155f500fa205c0bb5256e4
Andrew Chun <ay.chun@mail.utoronto.ca>
2022-08-04 11:40:37 -0400

    Add testMessageDateTime()

---
+++ test/MessageTest.java
@@ -0,0 +1,23 @@
+import entities.Message;
+import entities.Buyer;
+import entities.Seller;
+import org.junit.Test;
+
+public class MessageTest {
+
+    @Test
+    public void testMessageDateTime() {
+
+        Buyer sender = new Buyer("u1", "pw1");
+        Seller receiver = new Seller("u2", "pw2");
+
+        String testString = "03-08-2022 22:52:35";
+
+        Message m = new Message(sender, receiver, 123, "content", testString);
+
+        System.out.println(m);
+
+    }
+
+
+}
