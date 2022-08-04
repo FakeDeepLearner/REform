c6b72fd59b0b60927b6568a8120acb9166f3065c
Andrew Chun <ay.chun@mail.utoronto.ca>
2022-08-04 11:39:52 -0400

    Implement Comparable, update toString(), add dateTime

--- src/entities/Message.java
+++ src/entities/Message.java
@@ -1,28 +1,56 @@
 package entities;
-import entities.User;
 
-public class Message {
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+
+public class Message implements Comparable<Message> {
     private final User sender;
     private final User recipient;
     private final Integer messageID;
     private final String contents;
+    private final LocalDateTime datetime;
+    private final DateTimeFormatter formatter;
+
 
     /**
      * Create a message
-     * @param sender the sender of the message
+     *
+     * @param sender    the sender of the message
      * @param recipient the recipient of the message
      * @param messageID the id of the message
-     * @param contents the contents of the message
+     * @param contents  the contents of the message
      */
     public Message(User sender, User recipient, Integer messageID, String contents) {
         this.sender = sender;
         this.recipient = recipient;
         this.messageID = messageID;
         this.contents = contents;
+        this.datetime = LocalDateTime.now();
+        this.formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    }
+
+    public Message(User sender, User recipient, Integer messageID, String contents, String datetime) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.messageID = messageID;
+        this.contents = contents;
+
+        int year = Integer.parseInt(datetime.substring(6, 10));
+        int month = Integer.parseInt(datetime.substring(3, 5));
+        int dayOfMonth = Integer.parseInt(datetime.substring(0, 2));
+        int hour = Integer.parseInt(datetime.substring(11, 13));
+        int minute = Integer.parseInt(datetime.substring(14, 16));
+        int second = Integer.parseInt(datetime.substring(17, 19));
+
+        this.datetime = LocalDateTime.of(year, month, dayOfMonth, hour, minute, second);
+        this.formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
     }
 
     /**
      * Getter for the message ID
+     *
      * @return the message ID
      */
     public Integer getMessageID() {
@@ -31,6 +59,7 @@ public class Message {
 
     /**
      * Getter for the contents of the message
+     *
      * @return the message contents
      */
     public String getContents() {
@@ -39,6 +68,7 @@ public class Message {
 
     /**
      * Getter for the recipient of the message
+     *
      * @return the message's recipient
      */
     public User getRecipient() {
@@ -47,19 +77,31 @@ public class Message {
 
     /**
      * Getter for the sender of the message
+     *
      * @return the message's sender
      */
     public User getSender() {
         return sender;
     }
 
+    public LocalDateTime getDatetime() {
+        return datetime;
+    }
+
     /**
      * Format and print all the information about this message
+     *
      * @return a String containing all the details about the message
      */
+
     @Override
     public String toString() {
-        return "Message from: " + sender.getUsername() + " with an ID of " + messageID.toString() + "\n"
-                + contents + "\n";
+        return "\n" + datetime.format(formatter) + "\n" + "Message from: " + sender.getUsername() + " with an ID of " + messageID.toString() + "\n"
+                + contents;
+    }
+
+    @Override
+    public int compareTo(Message o) {
+        return this.datetime.compareTo(o.datetime);
     }
 }
