f72d0a24821356929f4a0671b4637fec6e0d60c4
Felix Zhang <felixfzhang@hotmail.com>
2022-08-07 20:10:00 -0400

    Add test for AuthenticateUser

---
+++ test/AuthenticateUserTest.java
@@ -0,0 +1,42 @@
+import entities.User;
+import entities.containers.UserContainer;
+import exceptions.UserBannedException;
+import org.junit.Test;
+import useCases.userUseCases.AuthenticateUser;
+import useCases.userUseCases.UserFactory;
+
+public class AuthenticateUserTest {
+    @Test
+    public void TestLogin() {
+        UserContainer<String, User> users = new UserContainer<>();
+        UserFactory userFactory = new UserFactory(users);
+
+        userFactory.createUser("ADMIN", "a1", "p1");
+
+        AuthenticateUser auth = new AuthenticateUser(users);
+
+        try {
+            auth.loginUser("a1", "p2");
+            assert !users.get("a1").isLoggedIn();
+
+            auth.loginUser("a1", "p1");
+            assert users.get("a1").isLoggedIn();
+
+        } catch(UserBannedException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void TestLogout() {
+        UserContainer<String, User> users = new UserContainer<>();
+        UserFactory userFactory = new UserFactory(users);
+
+        userFactory.createUser("ADMIN", "a1", "p1");
+        users.get("a1").setIsLoggedIn(true);
+
+        AuthenticateUser auth = new AuthenticateUser(users);
+        auth.logoutUser("a1");
+        assert !users.get("a1").isLoggedIn();
+    }
+}
